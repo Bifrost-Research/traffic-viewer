5754144ef725bc02bef2364951ae1bd9564e9914
ragondyn <ragondyn@gmail.com>
2016-12-19 19:40:55 +0000

    first implementation of a script to read videos from webcams using python and cv2

---
+++ python-investigation/webcam-flow.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 19 18:32:13 2016
+
+@author: ragondyn
+"""
+import numpy as np
+import cv2
+import os
+
+video_capture = cv2.VideoCapture(
+    "http://gieat.viewsurf.com/?id=5584&action=mediaRedirect")
+fps = (video_capture.get(cv2.CAP_PROP_FPS))
+alpha = 5 # spead-up the video
+b = True;
+
+while b:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Video', frame)
+    cv2.waitKey(int(np.floor(1/fps*1000*1/alpha)))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    b = video_capture.grab();
+
+video_capture.release()
+cv2.destroyAllWindows()
